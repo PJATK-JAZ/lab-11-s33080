f5106ddf3ecf9893de6bd5405f06fb52b4b09d98
Oliwia Ludwiczak <s33080@pjwstk.edu.pl>
2026-01-13 12:29:23 +0100

    test

--- Westeros/westeros-diagnostics-tools/src/main/java/com/westeros/diagnostics/runners/DiagnosticsRunner.java
+++ Westeros/westeros-diagnostics-tools/src/main/java/com/westeros/diagnostics/runners/DiagnosticsRunner.java
@@ -1,13 +1,21 @@
 package com.westeros.diagnostics.runners;
 
 import com.westeros.diagnostics.services.contract.Diagnostics;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class DiagnosticsRunner implements IRunDiagnoses {
+
+    private final List<IDiagnose> diagnostics;
 
     @Override
     public List<Diagnostics> runAll() {
-        return null;
+        return diagnostics.stream()
+                .map(IDiagnose::run)
+                .toList();
     }
 }
